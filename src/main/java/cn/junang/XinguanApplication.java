2d380b26de6f67a71258dcda89dd5028f05c9fea
Mr.s <1561971578@qq.com>
2020-07-19 18:18:02 +0800

    增加add包

--- src/main/java/cn/junang/XinguanApplication.java
+++ src/main/java/cn/junang/XinguanApplication.java
@@ -8,7 +8,6 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @SpringBootApplication
 @EnableSwagger2
 @MapperScan({"cn.junang.*.mapper","cn.junang.*.dao"})
-/*哈哈*/
 public class XinguanApplication {
 
     public static void main(String[] args) {
